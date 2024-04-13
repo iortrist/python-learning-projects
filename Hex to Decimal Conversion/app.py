92d952ce990b5e2c55ad8c6fe5770fe92306a1dd
Roi Tristan <roitristan@outlook.com>
2024-04-13 19:59:22 +0800

    Update

--- Hex to Decimal Conversion/app.py
+++ Hex to Decimal Conversion/app.py
@@ -1,22 +1,22 @@
 import os
 
-def hexToDecimal(hexNum) -> int:
+def hexToDecimal(hexNumber) -> int:
     hexNumbers = {
                     '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
                     'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
                     }
 
-    decimalNum = 0
-    hexLength = len(hexNum)-1
+    decimalNumber = 0
+    hexLength = len(hexNumber)-1
 
-    for i in hexNum:
-        if i.upper() not in hexNumbers:
-            return f'Invalid input. Please try again'
+    for char in hexNumber:
+        if char not in hexNumbers:
+            return f'Invalid hex number. Please try again'
         else:
-            decimalNum = decimalNum + (hexNumbers[i.upper()] * (16**hexLength))
+            decimalNumber = decimalNumber + (hexNumbers[char] * (16**hexLength))
             hexLength-= 1
             
-    return f"The hex value of {hexNum.upper()} is equivalent to {decimalNum} in decimal"
+    return f"The hex value of {hexNumber} is equivalent to {decimalNumber} in decimal"
 
 def clearConsole():
     if os.name == 'nt':
@@ -27,15 +27,15 @@ def clearConsole():
 
 convert = ''
 
-while convert.name == '':      
+while convert == '':      
   
     user_input = input("Enter the Hex you wish to convert: ")
 
     clearConsole()
 
-    print(hexToDecimal(user_input)+"\n") 
+    print(hexToDecimal(user_input.upper())+"\n") 
     
-    convert = input("Press Enter to convert another otherwise press any keys...   ")
+    convert = input("Press Enter to convert another otherwise press any keys and then Enter...   ")
     
     clearConsole()
 
